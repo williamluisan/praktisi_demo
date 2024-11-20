54e5c615ad50339d3df1da18f7b87d554c301a0a
DL William <dlprogrammer18@gmail.com>
2024-11-20 11:20:01 +0800

    feat: JWT generation

--- modules/auth.py
+++ modules/auth.py
@@ -1,15 +1,19 @@
 from fastapi import FastAPI, HTTPException
 import json
+from modules.token import Token
 
 class Auth:
     def login(req):
         with open('data/users.json', 'r') as file:
             users = json.load(file)
 
-        creds_matched = any(user for user in users if user['username'] == req.username and user['password'] == req.password)
+        user_data = next((user for user in users if user['username'] == req.username and user['password'] == req.password), None)
+
+        if user_data:
+            return Token.create_access_token(user_data)
         
-        if creds_matched:
-            return True
-        
-        if creds_matched == False:
+        if not user_data:
             raise HTTPException(status_code=401, detail="Wrong username or password")
+        
+    ###
+    # def authenticate():
---
+++ modules/token.py
@@ -0,0 +1,27 @@
+import jwt
+import time
+from dotenv import dotenv_values
+from pathlib import Path
+
+class Token:
+    def create_access_token(data):
+        dotenv_path = Path('./config/.env')
+        config = dotenv_values(dotenv_path) 
+
+        issued_at = int(time.time())
+
+        payload = {
+            'sub': data['id'], 
+            'iat': issued_at,
+            'detail': {
+                'name': data['name'],
+                'occupation': data['occupation']
+            },
+            'scope': [
+                'write:pdf',
+                'delete:pdf'    
+            ]
+        }
+        jwt_encoded = jwt.encode(payload, config["SECRET_KEY"], algorithm=config["ALGORITHM"])
+        
+        return jwt_encoded
